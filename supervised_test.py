27014c8b2b0e313b55c13fe4976143c041507d81
Varun Ved <varunv@zillowgroup.com>
2016-07-16 18:05:36 -0700

    supervised learning... or nah?

--- supervised_test.py
+++ supervised_test.py
@@ -1,6 +1,34 @@
 import tensorflow as tf
 import numpy as np
 import os
+from PIL import Image
+
+filesList = []
+dir = 'data/classical-sheet-music'
+
+for filename in os.listdir(dir):
+  filesList.append('data/classical-sheet-music/' +  str(filename))
+
+
+#comment out to loop through everything
+'''
+for files in l:
+  adjust(files)
+'''
+
+#don't really know what this was for
+
+'''
+print(filesList)
+
+filename_queue = tf.train.string_input_producer(filesList)
+reader = tf.WholeFileReader()
+
+#key,value = reader.read(filename_queue)
+
+#print(key,value)
+'''
+
 def adjust(filename):
   filename_queue = tf.train.string_input_producer([filename]) #  list of files to read
 
@@ -18,23 +46,16 @@ def adjust(filename):
     for i in range(1): #length of your filename list
       image = my_img.eval(session=sess) #here is your image Tensor :) 
 
-    print(image.shape)
-    print(np.squeeze(np.asarray(image)))
+    print(image[2,:0])
+    #print(np.average(image[:,1]))
+    #a = np.squeeze(image[:,2])
+    #print(a)
+    #print(np.squeeze(np.asarray(image)[0]))
 
     #Image.show(Image.fromarray(np.asarray(image)))
 
     coord.request_stop()
     coord.join(threads)
 
+adjust(filesList[0])
 
-dir = 'data/classical-sheet-music'
-l = []
-
-for filename in os.listdir(dir):
-  l.append('data/classical-sheet-music/' +  str(filename))
-
-adjust(l[0])
-'''
-for files in l:
-  adjust(files)
-'''
